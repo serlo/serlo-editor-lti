302e05492f2c17ed5c0ca29ed6eb3fd9da91b1cb
Hugo Tiburtino <hugotiburtino@protonmail.com>
2024-06-08 00:23:02 +0200

    chore(docker): fix restarting pod to pull new image

--- push-image-dev.sh
+++ push-image-dev.sh
@@ -3,4 +3,4 @@ set -e
 docker build . -f Dockerfile.dev -t eu.gcr.io/serlo-shared/editor-as-lti-tool:dev
 docker push eu.gcr.io/serlo-shared/editor-as-lti-tool:dev
 sleep 1
-kubectl delete pod -n editor -l app=editor
+kubectl delete pod -n editor -l app=editor-as-lti-tool
