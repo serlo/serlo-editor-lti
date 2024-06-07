39bd0b2f3e8b518866558e72efd2d15aedb745c0
Hugo Tiburtino <hugotiburtino@protonmail.com>
2024-06-07 23:48:57 +0200

    chore(docker): add nvim to dev image and redeploy after pushing new dev image

--- push-image-dev.sh
+++ push-image-dev.sh
@@ -1,2 +1,4 @@
 docker build . -f Dockerfile.dev -t eu.gcr.io/serlo-shared/editor-as-lti-tool:dev
 docker push eu.gcr.io/serlo-shared/editor-as-lti-tool:dev
+sleep 1
+kubectl delete pod -n editor -l app=editor
