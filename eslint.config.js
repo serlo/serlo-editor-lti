da518855a4e69b01a02b1abb54d6da7f7c1e685a
Stephan Kulla <git.mail@kulla.me>
2024-07-18 21:05:56 +0200

    chore(eslint): Use jsx-runtime version

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import pluginReactConfig from 'eslint-plugin-react/configs/jsx-runtime.js'
 import { fixupConfigRules } from '@eslint/compat'
 
 const ourCustomConfig = {
