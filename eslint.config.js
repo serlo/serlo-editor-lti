2c8dbdbe39fff9481be8f095f9a0917c79b8e61e
Stephan Kulla <git.mail@kulla.me>
2024-07-18 21:41:48 +0200

    doc(eslint): Document eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,33 +7,39 @@ import pluginReactRefresh from 'eslint-plugin-react-refresh'
 import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
 
 const ourCustomConfig = {
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  // Add our custom rules here
   rules: {
     'no-console': 'warn',
   },
 }
 
 export default [
+  // General settings
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+
+  // Recommended settings for JavaScript and TypeScript
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
+  // Recommended settings for React
+  { settings: { react: { version: 'detect' } } },
   ...fixupConfigRules(pluginReactConfig),
+
+  // Plugin for linting React Hooks
   {
     plugins: { 'react-hooks': fixupPluginRules(pluginReactHooks) },
     rules: pluginReactHooks.configs.recommended.rules,
   },
+
+  // Plugin for validating that components can safely be updated with fast refresh.
   {
     plugins: { 'react-refresh': fixupPluginRules(pluginReactRefresh) },
     rules: {
       'react-refresh/only-export-components': 'warn',
     },
   },
+
+  // Our custom rules
   ourCustomConfig,
 ]
