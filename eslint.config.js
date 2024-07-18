9e59413bb9934da88a53f559e3db51a605911323
Stephan Kulla <git.mail@kulla.me>
2024-07-18 21:05:23 +0200

    chore(eslint): Update to eslint 9.x

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import { fixupConfigRules } from '@eslint/compat'
+
+const ourCustomConfig = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Add our custom rules here
+    'no-console': 'error',
+  },
+}
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  ourCustomConfig,
+]
