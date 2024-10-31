f2ec7d8c767651cfe247e3a6695e11b743019594
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-10-31 17:14:32 +0100

    refactor(edusharing): unite code to jwt with same scope

--- src/backend/edusharing/jwt-helpers.ts
+++ src/backend/edusharing/jwt-helpers.ts
@@ -1,11 +1,12 @@
-import jwt, { VerifyOptions } from 'jsonwebtoken'
+import { KeyObject } from 'crypto'
+import jwt from 'jsonwebtoken'
 import JWKSClient, { JwksClient } from 'jwks-rsa'
 
 const jwksClients: Record<string, JwksClient | undefined> = {}
 
 export function verifyJwt(args: {
   token: string
-  verifyOptions: VerifyOptions
+  verifyOptions: jwt.VerifyOptions
   keysetUrl: string
 }): Promise<
   | { success: true; decoded: jwt.JwtPayload }
@@ -75,4 +76,47 @@ export function verifyJwt(args: {
       }
     }
   })
+}
+
+const defaultExpireAfterSeconds = 15
+
+export function signJwtWithBase64Key({
+  payload,
+  keyid,
+  privateKey,
+  expireAfterSeconds,
+}: {
+  payload: Omit<jwt.JwtPayload, 'iat'>
+  keyid: string
+  privateKey: KeyObject
+  expireAfterSeconds?: number
+}) {
+  return signJwt({
+    payload,
+    keyid,
+    privateKey,
+    expireAfterSeconds,
+  })
+}
+
+function signJwt({
+  payload,
+  keyid,
+  privateKey,
+  expireAfterSeconds = defaultExpireAfterSeconds,
+}: {
+  payload: Omit<jwt.JwtPayload, 'iat'>
+  keyid: string
+  privateKey: KeyObject
+  expireAfterSeconds?: number
+}) {
+  return jwt.sign(
+    { ...payload, iat: Math.floor(Date.now() / 1000) },
+    privateKey,
+    {
+      algorithm: 'RS256',
+      expiresIn: expireAfterSeconds,
+      keyid,
+    }
+  )
 }
