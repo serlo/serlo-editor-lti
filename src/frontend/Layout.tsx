57b89792f6ccb1e6becf279e30a9b0c25070268d
Lars <lars@serlo.org>
2024-11-12 16:03:16 +0100

    fix: set max width for content

---
+++ src/frontend/Layout.tsx
@@ -0,0 +1,30 @@
+// Centered & max-width content layout
+export function Layout({ children }: { children: React.ReactNode }) {
+  const maxContentWidth = '56rem'
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+      }}
+    >
+      <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
+      <main
+        style={{
+          flexGrow: 1,
+          flexShrink: 1,
+          flexBasis: maxContentWidth,
+          maxWidth: `min(100%, ${maxContentWidth})`,
+          // Make horizontal scroll bar appear on small width. Plugin menu, plugin toolbar, ... need some space.
+          minWidth: '40rem',
+          // Leave some space for editor UI that extends beyond (plugin toolbar, drag handle, ...)
+          padding: '3rem',
+        }}
+      >
+        {children}
+      </main>
+      <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
+    </div>
+  )
+}
