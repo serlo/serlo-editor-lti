56c9c79cb49a4326af5134913f19b6a02c8808b6
Botho <1258870+elbotho@users.noreply.github.com>
2024-11-12 18:11:36 +0100

    chore(media): remove unused code

--- e2e/tests/media.ts
+++ e2e/tests/media.ts
@@ -1,19 +1,4 @@
-import { HeadObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import config from '../../src/utils/config'
-
 Feature('Media upload and proxy')
-
-const s3Client = new S3Client({
-  region: config.BUCKET_REGION,
-  credentials: {
-    accessKeyId: config.BUCKET_ACCESS_KEY_ID,
-    secretAccessKey: config.BUCKET_SECRET_ACCESS_KEY,
-  },
-  endpoint: config.S3_ENDPOINT,
-  forcePathStyle: true,
-})
-
-const uploadedKeys: string[] = []
 
 Scenario('Requesting media proxy with example image works', ({ I }) => {
   I.sendGetRequest('http://localhost:3000/media/four-byte-burger.png')
