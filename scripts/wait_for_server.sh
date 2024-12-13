a5615f975b278eed6d2e27c58da2c4e1dd624dbd
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-12-13 23:43:38 +0100

    chore(ci): be sure server is really ready
    It could before call test even though the a platform is not
    registered.

--- scripts/wait_for_server.sh
+++ scripts/wait_for_server.sh
@@ -1,4 +1,6 @@
-if ! timeout 120 bash -c 'while [ ! -f output.log ] || ! grep -m 1 "Registered platform:" output.log; do sleep 1; done'; then
+LATEST_LOG_BEFORE_READY="Registered platform: itslearning-mock"
+
+if ! timeout 120 bash -c "while [ ! -f output.log ] || ! grep -m 1 \"$LATEST_LOG_BEFORE_READY\" output.log; do sleep 1; done"; then
     echo "Error: Server is not working correctly." >&2
     exit 1
 fi
