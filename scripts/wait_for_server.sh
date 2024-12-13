be70000ffeec2cc2dbc74b050e7dd09af178bbd6
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2024-12-13 23:33:52 +0100

    chore(ci): check file first before grepping

--- scripts/wait_for_server.sh
+++ scripts/wait_for_server.sh
@@ -1,4 +1,4 @@
-if ! timeout 120 bash -c 'while ! grep -m 1 "Registered platform:" output.log; do sleep 1; done'; then
+if ! timeout 120 bash -c 'while [ ! -f output.log ] || ! grep -m 1 "Registered platform:" output.log; do sleep 1; done'; then
     echo "Error: Server is not working correctly." >&2
     exit 1
 fi
