7e0fec275c3fa17d451724ef038dc67b004f7b97
Botho <1258870+elbotho@users.noreply.github.com>
2025-01-31 12:03:48 +0100

    adapt header layout, add back button

--- src/frontend/Header.tsx
+++ src/frontend/Header.tsx
@@ -1,28 +1,40 @@
+import EditorLogoLight from './assets/serlo-editor-logo-light.svg'
+
 export default function Header() {
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString)
   const title = urlParams.get('title') ?? 'Inhalt'
-  const contextTitle = urlParams.get('contextTitle')
+
   return (
     <div
       style={{
         display: 'flex',
-        flexDirection: 'row',
-        paddingTop: '1rem',
+        padding: '1rem',
         alignItems: 'center',
         gap: '1rem',
+        boxShadow: '0px 0px 10px 0px rgba(0,0,0,.22)',
+        marginBottom: '0.5rem',
       }}
     >
       <img
-        style={{ width: '2.8rem', height: '2.8rem' }}
-        src="https://editor.serlo.dev/media/serlo-org/skkwa1vksa3v2yc7bj9z0bni/image.png"
+        style={{ width: '3.5rem', height: '3.5rem' }}
+        src={EditorLogoLight}
       />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        {contextTitle ? (
-          <div style={{ color: '#666666', fontSize: '0.9rem' }}>
-            {contextTitle}
-          </div>
-        ) : null}
+        <div>
+          <a
+            onClick={() => history.go(-1)}
+            style={{ color: '#007EC1', fontSize: '0.9rem', cursor: 'pointer' }}
+          >
+            <span style={{ rotate: '180deg', display: 'inline-block' }}>⮕</span>{' '}
+            Zurück zu Moodle
+          </a>{' '}
+          {/* {contextTitle ? (
+            <span style={{ color: '#666', fontSize: '0.9rem' }}>
+              ({contextTitle})
+            </span>
+          ) : null} */}
+        </div>
         <h2 style={{ fontSize: '1.25rem' }}>{title}</h2>
       </div>
     </div>
--- src/frontend/Layout.tsx
+++ src/frontend/Layout.tsx
@@ -9,38 +9,41 @@ export function Layout({ children }: { children: React.ReactNode }) {
   // Leave some space for editor UI that extends beyond (plugin toolbar, drag handle, ...)
   const paddingAroundContent = '3rem'
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        // Make horizontal scroll bar appear on small width. Plugin menu, plugin toolbar, ... need some space.
-        minWidth: '40rem',
-        overflowX: 'auto',
-      }}
-    >
-      <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
-      <main
+    <>
+      {showHeader ? <Header /> : null}
+
+      <div
         style={{
-          flexGrow: 1,
-          flexShrink: 1,
-          flexBasis: maxContentWidth,
-          maxWidth: `min(100%, ${maxContentWidth})`,
-          paddingLeft: paddingAroundContent,
-          paddingRight: paddingAroundContent,
+          display: 'flex',
+          flexDirection: 'row',
+          backgroundColor: 'white',
+          // Make horizontal scroll bar appear on small width. Plugin menu, plugin toolbar, ... need some space.
+          minWidth: '40rem',
+          overflowX: 'auto',
         }}
       >
-        {showHeader ? <Header /> : null}
-        <div
+        <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
+        <main
           style={{
-            paddingTop: paddingAroundContent,
-            paddingBottom: paddingAroundContent,
+            flexGrow: 1,
+            flexShrink: 1,
+            flexBasis: maxContentWidth,
+            maxWidth: `min(100%, ${maxContentWidth})`,
+            paddingLeft: paddingAroundContent,
+            paddingRight: paddingAroundContent,
           }}
         >
-          {children}
-        </div>
-      </main>
-      <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
-    </div>
+          <div
+            style={{
+              paddingTop: paddingAroundContent,
+              paddingBottom: paddingAroundContent,
+            }}
+          >
+            {children}
+          </div>
+        </main>
+        <aside style={{ flexGrow: 1, flexShrink: 1, flexBasis: 0 }}></aside>
+      </div>
+    </>
   )
 }
